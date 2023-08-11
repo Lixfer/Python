f63ea76c0952849d1b36e252d5e7938f36c81556
Krzysztof <krzysztof.kubiak.pl@gmail.com>
2023-08-11 12:18:52 +0200

    Exercise

--- app.py
+++ app.py
@@ -32,9 +32,31 @@
 # print(increment(2, 5))
 
 
-def multiply(*numbers):
-    for number in numbers:
-        print(number)
+# def multiply(*numbers):
+#     total = 1
+#     for number in numbers:
+#         total *= number
+#     return total
 
 
-multiply(2, 3, 4, 5)
+# print(multiply(2, 3, 4, 5))
+
+
+# def save_user(**user):
+#     print(user["name"])
+
+
+# save_user(id=1, name="John", age=22)
+
+
+def fizz_buzz(input):
+    if (input % 3 == 0) and (input % 5 == 0):
+        return "FizzBuzz"
+    if input % 3 == 0:
+        return "Fizz"
+    if input % 5 == 0:
+        return "Buzz"   
+    return input
+
+
+print(fizz_buzz(15))
